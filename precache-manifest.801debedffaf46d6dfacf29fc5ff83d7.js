28df5981000f34b28fb786a8cad157bdce7a2317
swoodo <tictaks@gmail.com>
2019-12-04 17:46:59 -0800

    Updates

---
+++ precache-manifest.801debedffaf46d6dfacf29fc5ff83d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883a438f4d492dd00093f4c4b23587d1",
+    "url": "/random-quote-machine-fcc/index.html"
+  },
+  {
+    "revision": "42632da7404004059ff3",
+    "url": "/random-quote-machine-fcc/static/css/main.3c3ba85e.chunk.css"
+  },
+  {
+    "revision": "2a3f115c749a6170ac2f",
+    "url": "/random-quote-machine-fcc/static/js/2.c86e4d28.chunk.js"
+  },
+  {
+    "revision": "42632da7404004059ff3",
+    "url": "/random-quote-machine-fcc/static/js/main.56cb64f3.chunk.js"
+  },
+  {
+    "revision": "9a8de1045e3e28e993e0",
+    "url": "/random-quote-machine-fcc/static/js/runtime-main.eb619e67.js"
+  },
+  {
+    "revision": "a88c52de185d8b002e918dd7e7c25367",
+    "url": "/random-quote-machine-fcc/static/media/akbar.a88c52de.ttf"
+  },
+  {
+    "revision": "c18d7493fe13e41260ea3fba11c0f41f",
+    "url": "/random-quote-machine-fcc/static/media/homer-1.c18d7493.svg"
+  },
+  {
+    "revision": "b2d4d33e8e4bb86365679e31e13d4a8a",
+    "url": "/random-quote-machine-fcc/static/media/homer-2.b2d4d33e.svg"
+  },
+  {
+    "revision": "ddb14a217233ba0fcad246a5a7b02c76",
+    "url": "/random-quote-machine-fcc/static/media/homer-3.ddb14a21.svg"
+  }
+]);
