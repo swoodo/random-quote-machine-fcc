de2fde63b58887098db3f16bb19fd8b17ad39041
swoodo <tictaks@gmail.com>
2019-12-05 18:00:51 -0800

    spinner and bg transitions

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import homer1 from './images/homer-1.svg';
 import homer2 from './images/homer-2.svg';
 import homer3 from './images/homer-3.svg';
 import homerQuotes from './quotes.json';
+import spinner from './images/spinner.gif';
 import './App.css';
 
 class App extends Component {
@@ -55,10 +56,14 @@ class App extends Component {
     let newQuote = this.newQuote();
     
     document.body.style.pointerEvents = 'none';
+    document.querySelector(".quote-text").style.opacity = 0;
+    document.querySelector(".spinner").style.opacity = 1;
     setTimeout(function() {
       document.querySelector('.quote-text').style.display= 'block';
       document.querySelector('.quote-text').innerHTML = newQuote;
       document.body.style.pointerEvents = 'initial';
+      document.querySelector(".quote-text").style.opacity = 1;
+      document.querySelector(".spinner").style.opacity = 0;
     }, 1000);
 
     //setstate
@@ -90,7 +95,13 @@ class App extends Component {
     let bgImg = homerQuotes[randomNum]["image"];
     let bgImgStr = 'linear-gradient(0deg, #ffffff, #ffffff, transparent), url('+bgImg+') no-repeat center';
 
-    document.querySelector('.quote-box-image').style.background=bgImgStr;
+    let image = document.querySelector('.quote-box-image');
+    image.style.opacity = 0;
+    setTimeout(function() {
+      image.style.background = bgImgStr;
+      image.style.opacity = 0.2;
+    }, 1000);
+    
 
     this.setState({
       quoteIndex: randomNum,
@@ -111,6 +122,7 @@ class App extends Component {
         <div className="container content-container">
           <div className="content">
             <div id="quote-box">
+              
               <div class="quote-box-image"></div>
             <svg className="quote-symbol noselect">
               <path d="M20.7,26.1c0,2.4-1.3,3.6-4,3.6c-1.8,0-5-3.7-9.7-11C2.4,11.4,0.1,6.6,0,4.2
@@ -118,7 +130,9 @@ class App extends Component {
               M38.6,21.1c0,0.9-0.6,1.7-1.7,2.3c-1.1,0.6-2.2,1-3.3,1c-0.8,0.1-2.5-1.6-5.3-5c-3.1-3.8-4.7-7.2-4.7-10c0-1.4,1-2.2,2.9-2.4
               c1,0,3.3,2.1,6.7,6.2C36.5,17.4,38.4,20,38.6,21.1z"/>
             </svg>
-            <div id="text"><p className="quote-text">{this.state.quote}</p></div>
+            <div id="text">
+              <div className="spinner"><img src={spinner} alt="loading spinner" /></div>
+              <p className="quote-text">{this.state.quote}</p></div>
             <div id="author"><p>- Homer Jay Simpson</p></div>
             <a id="tweet-quote" href="http://www.twitter.com/intent/tweet" onClick={this.tweetClick} target="_blank" rel="noopener noreferrer" >
               <button className="btn">Tweet</button>
